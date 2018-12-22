0f2c4735a4afbc33c99cda919ad30fc36f532a60
Ethan Vernon <ethan.vernon@colorado.edu>
2018-12-22 16:54:17 +0800

    Added LevelChooser.js

---
+++ src/LevelChooser.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './App.css';
+
+export class LevelChooser extends Component {
+  	constructor(props) {
+	    super(props);
+	    this.handleLevel = this.handleLevel.bind(this);
+	}
+
+	handleLevel(e) {
+    	this.props.onChange(parseInt(e.target.value));
+	}
+
+
+	render() {
+		return (
+			<div>
+				<span style={{fontSize:16, marginRight:10}}>Choose your level</span>
+				<input 
+					style={{textAlign:'center'}}
+		        	id="clericLevel"
+		        	type="number"
+		        	min="1"
+		        	max="4"
+		        	defaultValue={this.props.level} 
+		        	onInput = {this.handleLevel}/> 
+			</div>
+		);
+	}
+}
--- src/Parent.js
+++ src/Parent.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react';
 import './App.css';
+import {LevelChooser} from './LevelChooser';
 
 export class Parent extends Component {
+
+	constructor(props) {
+	    super(props);
+
+	    this.state = {
+	    	cantrips: 		['Guidance', 'Light', 'Mending', 'Resistance', 'Sacred Flame', 'Spare the Dying', 'Thaumaturgy'],	    	
+	     	firstLevel: 	['Bane', 'Bless', 'Command', 'Create or Destroy Water', 'Cure Wounds', 'Detect Evil and Good', 'Detect Magic', 'Detect Poison and Disease', 'Guiding Bolt', 'Healing Word', 
+	      					'Inflict Wounds', 'Protection from Evil and Good', 'Purify Food and Drink', 'Sanctuary', 'Shield of Faith'],
+	      	secondLevel: 	['Aid', 'Augury', 'Blindness/Deafness', 'Calm Emotions', 'Continual Flame', 'Enhance Ability', 'Find Traps', 'Gentle Repose', 'Hold Person', 'Lesser Restoration', 
+	      					'Locate Object', 'Prayer of Healing', 'Protection from Poison', 'Silence', 'Spiritual Weapon', 'Warding Bond', 'Zone of Truth'],
+	      	clericLevel: 1
+	    };
+
+	    this.changeClericLevel = this.changeClericLevel.bind(this);
+	}
+
+	changeClericLevel(newLevel) {
+		this.setState({
+			clericLevel: newLevel
+		})
+	}
 
 
 	render() {
 		return (
-			<h1>For Ale</h1>
+			<div>
+				<h2 style={{marginBottom:0}}>Spell Selector</h2>
+				<h4 style={{marginTop:0}}>Level {this.state.clericLevel} Cleric</h4>
+				<LevelChooser
+					onChange = {this.changeClericLevel}
+					level={this.state.clericLevel}/>
+			</div>
 			)
 	}
 }
